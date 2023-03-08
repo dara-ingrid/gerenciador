9b02f354feb59497fa8878b7d90ee8b309d4f952
Dara Ferreira <dara-ingrid@hotmail.com>
2023-03-08 15:00:28 -0300

    Páginas dinâmicas com JSP

--- src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
+++ src/br/com/alura/gerenciador/servlet/ListaEmpresasServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,16 +24,12 @@ public class ListaEmpresasServlet extends HttpServlet {
 		Banco banco = new Banco();
 		List<Empresa> lista = banco.getEmpresas();
 		
-		PrintWriter out = response.getWriter();
-		out.println("<html><body>");
-		out.println("<ul>");
 		
-		for (Empresa empresa : lista) {
-			out.println("<li>" + empresa.getNome() + "</li>");
-		}
+		request.setAttribute("empresas", lista);
+		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas.jsp");
 		
-		out.println("</ul>");
-		out.println("</body></html>");
+		
+		rd.forward(request, response);
 	}
 
 }
